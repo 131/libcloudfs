43cd325888609cfdb3ddbe2b1e926dda81d3baa5
François Leurent <131.js@cloudyks.org>
2019-08-19 23:16:49 +0200

    Align to casfs API

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ class cloudfs extends casfs {
     return sprintf("%s/%s/%s", hash.substr(0, 2), hash.substr(2, 1), hash);
   }
 
-  _cas_open(block_hash) {
-    let block_path = this.block_path(block_hash);
+  _cas_read(inode) {
+    let block_path = this.block_path(inode.block_hash);
     var remoteUrl  = Storage.tempURL(this.dst_ctx, this.dst_container, block_path);
     return new RandomReadHTTP(remoteUrl, {
       MAX_BL_SIZE : 100 * 1024 * 1024,

7391b93291de860905d1ca42adf37381b970b2ed
François Leurent <131.js@cloudyks.org>
2019-09-29 14:05:45 +0200

    Fix startup sequence

--- dist/start.cmd
+++ dist/start.cmd
@@ -1,2 +1,2 @@
 set DEBUG=random-read-http:info,osqlite:swift:info,casfs:info,random-read-http:error,osqlite:swift:error,casfs:error
-node node_modules/cnyks/bin/cnyks cloudfs/bin --ir://run=simple_ro --config_path=config/config.json
+node node_modules/cnyks/bin/cnyks ./bin --ir://run=simple_ro --config_path=config/config.json
